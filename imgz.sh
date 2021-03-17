df73897494fbcc12065b6a15076c552824ee95e8
Margus <marguslanto@gmail.com>
2021-03-17 20:53:25 +0200

    echo all images in folder

--- imgz.sh
+++ imgz.sh
@@ -1,1 +1,13 @@
-echo Hello world!
+#! /bin/bash
+
+images=$(echo *.{png,PNG,jpg,JPG})
+
+for image in $images
+do
+  # skip if filename starts with *
+  if [[ `echo $image | cut -c1-1` == '*' ]]; then
+    continue
+  fi
+
+  echo $image
+done
